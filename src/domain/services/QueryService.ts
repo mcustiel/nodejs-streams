4d308b698e118e337b88f458c73e9be76d47a3cb
Mariano Custiel <jmcustiel@gmail.com>
2022-09-12 07:09:35 +0200

    Update QueryService.ts

--- src/domain/services/QueryService.ts
+++ src/domain/services/QueryService.ts
@@ -5,6 +5,9 @@ import Repository from "../repositories/Repository";
 export default class QueryService {
     public constructor(private readonly repository: Repository) { }
 
+    // Right now this is very simple, looks almost unneecessary. But it provides
+    // a separation between the presentation and the persistence layers and could 
+    // be more complex logic-wise.
     public searchRepositories(query: Query): Promise<Readable> {
         return this.repository.search(query);
     }
